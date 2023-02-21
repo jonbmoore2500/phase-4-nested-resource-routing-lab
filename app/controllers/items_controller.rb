5161c681d53f5d010bb304385a23cc6ca2687fa6
Jonathan <jonathanmoorevla@gmail.com>
2023-02-20 19:58:31 -0600

    Pass tests to index nested items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,23 @@
 class ItemsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   def index
+  if params[:user_id]
+    user = User.find_by!(id: params[:user_id])
+    items = user.items
+  else
     items = Item.all
+  end
     render json: items, include: :user
   end
+  private
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
+  end
+  # def index
+  #   items = Item.all
+  #   render json: items, include: :user
+  # end
 
 end
